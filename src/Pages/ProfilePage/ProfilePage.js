fa544d97493ba663548f467dbeab37887c3733bc
Adam Morris <ah.morris@outlook.com>
2021-08-15 20:39:16 +0100

    cleaned up code inside profilePage - Adam

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -1,52 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Languages from "../../Components/Languages/Languages";
 import Avatar from "../../Components/UI/Avatar/Avatar";
+import CoverImage from "../../Components/UI/CoverImage/CoverImage";
+import ProfileData from "../../Components/UI/ProfileData/ProfileData";
 import classes from "./ProfilePage.module.scss";
-
-/* 
-
-
-
-THIS IS JUST A MOCK UP PAGE FOR NOW
-NEEDS WORK
-
-
-
-*/
 
 const ProfilePage = (props) => {
   const user = useSelector((state) => state.auth.user);
-
   return (
     <div className={classes.ProfilePage}>
       <div className={classes.ProfilePage__ProfileData}>
-        <section className={classes.CoverImage}>
-          <label htmlFor="coverImage">Upload an image</label>
-          <input type="file" id="coverImage" hidden />
-        </section>
-        <section className={classes.Avatar}>
-          <div className={classes.Avatar__Container}>
-            <Avatar className={classes.Avatar__Image} />
-            <div className={classes.Data}>
-              <p>{`${user.userData.firstName} ${user.userData.lastName}`}</p>
-              <p className={classes.FriendsAmount}>233 Connections</p>
-              <div className={classes.Friends}>
-                <Avatar className={classes.Friend} />
-                <Avatar className={classes.Friend} />
-                <Avatar className={classes.Friend} />
-                <Avatar className={classes.Friend} />
-              </div>
+        <CoverImage />
+        <section className={classes.Profile__Top__Container}>
+          <div className={classes.Profile__Top__Data}>
+            <div className={classes.Profile__Top__Data__Left}>
+              <Avatar className={classes.Avatar__Image} />
+              <ProfileData user={user} />
+            </div>
+            <div className={classes.Profile__Top__Data__Right}>
+              <p>{user.userData.reputation} Reputation Points</p>
+              <p>{user.userData.likes.length} Liked Posts</p>
+              <p>{user.userData.comments} Comments</p>
             </div>
           </div>
-          <div className={classes.Languages}>
-            {user.userData.toolbox.map((tool) => {
-              return (
-                <div className={classes.Language}>
-                  <p>{tool.label}</p>
-                </div>
-              );
-            })}
-          </div>
+          <Languages user={user} />
         </section>
       </div>
     </div>
