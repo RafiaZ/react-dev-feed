7c26bc3d29dbb3cb27b94cd0172ec257b857a633
Adam Morris <ah.morris@outlook.com>
2021-08-15 19:44:54 +0100

    added user-account-page.js

---
+++ src/Pages/UserAccountPage/UserAccountPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./UserAccountPage.module.scss";
+
+const UserAccountPage = (props) => {
+  return (
+    <div>
+      <h1>USERS ACCOUNT</h1>
+    </div>
+  );
+};
+
+export default UserAccountPage;
